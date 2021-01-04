0067350d2713acb4819cff3df93a632a2233ff18
Carlos Humberto Marin Murillo <chmarin@bancolombia.com.co>
2021-01-04 15:51:32 -0500

    Modificación de proyecto inicial para crear clases relacionadas con las ruletas

---
+++ Roulette.Model/Roulette/Bet.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+
+namespace Roulette.Model
+{
+    public class Bet
+    {
+        public int RouletteId { get; set; }
+        public BetTypeEnum BetType { get; set; }
+        public int Number { get; set; }
+        public ColorEnum Color { get; set; }
+        [Range(1, 10000, ErrorMessage = "Value must be between $1 and $10000")]
+        public decimal BetValue { get; set; }
+        public decimal BetEarnings { get; set; }
+    }
+}
---
+++ Roulette.Model/Roulette/ColorEnum.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Roulette.Model
+{
+    public enum ColorEnum
+    {
+        Black,
+        Red
+    }
+}
