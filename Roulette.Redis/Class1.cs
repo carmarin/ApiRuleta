3749c20ad1142d0c6cfc4f64be1ee5e206c66868
Carlos Humberto Marin Murillo <chmarin@bancolombia.com.co>
2021-01-04 18:41:56 -0500

    Ajustes Repositorio Redis

--- Roulette.Redis/Class1.cs
+++ Roulette.Redis/Class1.cs
@@ -1,8 +1,35 @@
-﻿using System;
+﻿using Roulette.Model.Gateways;
+using Roulette.Model;
+using System;
+using System.Collections.Generic;
 
 namespace Roulette.Redis
 {
-    public class Class1
+    public class RouletteRepository : IRouletteRepository
     {
+        public void AddBet(Bet bet)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Model.Roulette> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Model.Roulette GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Save(Model.Roulette roulette)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Model.Roulette roulette)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
